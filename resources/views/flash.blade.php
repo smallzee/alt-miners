fe95699ac585bf493f34da3621c9b9de3c725631
Tech4All <tech4all583@gmail.com>
2021-05-11 17:04:47 +0100

    initial

---
+++ resources/views/flash.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tech4all
+ * Date: 2021-05-11
+ * Time: 17:00
+ */
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", "MyController@index")->name('index');
+

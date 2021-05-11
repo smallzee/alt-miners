5b7486a372acb7ecfe982d4fcdfcb2d098d14598
Tech4All <tech4all583@gmail.com>
2021-05-11 17:11:16 +0100

    initial

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,62 @@
 <!Doctype html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Finally a safe and transparent environment for bitcoin mining. {{ get_settings('web_title') }} offers you the opportunity to jump in the Crypto Mining industry.">
+
     <title>{{ page_title($page_title) }}</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/color-switcher.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/ion.rangeSlider.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/ion.rangeSlider.skinFlat.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="assets/extras/settings.css">
+    <link rel="stylesheet" type="text/css" href="assets/extras/layers.css">
+    <link rel="stylesheet" type="text/css" href="assets/extras/navigation.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
 </head>
 <body>
 
 
 
+<script src="assets/js/jquery-min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 
+<script src="assets/js/jquery.mixitup.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/ion.rangeSlider.js"></script>
+<script src="assets/js/jquery.parallax.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/wow.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.slicknav.js"></script>
+<script src="assets/js/nivo-lightbox.js"></script>
+<script src="assets/js/jquery.slicknav.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/form-validator.min.js"></script>
+<script src="assets/js/contact-form-script.min.js"></script>
+<script src="assets/js/jquery.themepunch.tools.min.js"></script>
+<script src="assets/js/jquery.themepunch.revolution.min.js"></script>
+
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.actions.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.carousel.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.migration.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.parallax.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="assets/js/extensions/revolution.extension.video.min.js"></script>
 </body>
 </html>
